551f3f05ce22e112512f8199f754608d6396c436
Cyril <cfdev161@gmail.com>
2015-09-22 09:36:03 +0200

    Add dev version codename

--- src/mcercle.h
+++ src/mcercle.h
@@ -22,7 +22,7 @@
 namespace MCERCLE {
 // Version et version de bdd supporte
 	const QString Version = "15.06";
-	const QString CodeName = "Thunderstorm";
+	const QString CodeName = "dev";
 	const int Dbase_support = 8;
 
 // Type paiement: espece, cheque, CB, TIP, virement, prelevement, autre
